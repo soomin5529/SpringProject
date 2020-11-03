102d6cb1d5f1c12a268adc39e6c56b816492beed
UngiKang <kangmis1184@gmail.com>
2020-11-03 19:51:59 +0900

    강운기 커밋 : 임시 커밋

--- src/controller/DashBoardController.java
+++ src/controller/DashBoardController.java
@@ -1,10 +1,17 @@
 package controller;
 
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,97 +20,152 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import area.AreaDTO;
-import industry.IndustryDTO;
-import industry.IndustryRankDTO;
+import area.AreaLikeDTO;
+import area.DongDTO;
+import area.SigunguDTO;
+import dashboard.IndustryCountDTO;
+import industry.MainCategoryDTO;
 import service.AreaDAO;
+import service.AreaLikeDAO;
+import service.BoardLikeDAO;
+import service.DashboardDAO;
 import service.IndustryDAO;
+import service.MemberDAO;
 import service.StoreDAO;
-import store.StoreDTO;
 
 @Controller
-@RequestMapping("/dashBoard/")
-public class DashBoardController {
-
+@RequestMapping("/dashboard/")
+public class DashboardController {
 	ModelAndView mv;
-
+	String userid = "";
+	String name = "";
+	
+	@Autowired
+	DashboardDAO dashboardDB;
+	
 	@Autowired
 	AreaDAO areaDB;
+	
 	@Autowired
 	IndustryDAO industryDB;
 	@Autowired
-	StoreDAO storeDB;
+	AreaLikeDAO arealikeDB;
+	
+	@ModelAttribute
+	public void headProcess(HttpServletRequest request, HttpServletResponse res) {
+		try {
+			request.setCharacterEncoding("utf-8");
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		HttpSession session = request.getSession();
+		if (request.getParameter("userid") != null) {
+			session.setAttribute("userid", request.getParameter("userid"));
+		}
+		userid = (String) session.getAttribute("userid");
+	}
 
-	@RequestMapping(value = "/dong/{dongCode}", method = RequestMethod.POST, produces = "application/text; charset=utf8")
+
+	/* 대분류 업종 top3, 중분류 업종 top5, 주요시설 수 */
+	@RequestMapping(value = "chart", method = RequestMethod.POST, produces = "application/json; charset=utf8")
 	@ResponseBody
-	public ModelAndView openDashBoard(@PathVariable("dongCode") String dongCode) {
+	public List<IndustryCountDTO> chart(@RequestBody Map<String, String> dong_code) throws Throwable {
+		List<IndustryCountDTO> result = dashboardDB.getMainCategoryCount(dong_code);
+		result.addAll(dashboardDB.getMiddleCategoryCount(dong_code));
+		result.addAll(dashboardDB.getFacilityCount(dong_code));
+		return result;
+	}
+	
+	@RequestMapping(value = "{dongCode}" , produces = "application/text; charset=utf8")
+	@ResponseBody
+	public ModelAndView openDashBoardOfDong(@PathVariable("dongCode") String dongCode) throws Throwable {
 		mv = new ModelAndView();
-		// 시군구 읍면동 이름
-		// 차트
+		String dongName = "";
+		String sigunguName = "";
+		AreaDTO sigungu = null;
+		AreaDTO dong=null;
+
 		String sigunguCode = dongCode.substring(0, 5);
-
-		// 행정구역(시군구, 읍면동) 이름
-		AreaDTO sigungu = areaDB.sigungu(sigunguCode);
-		AreaDTO dong = areaDB.dong(dongCode);
-		// 선택된 동에 있는 maincategory (산업-대분류)
-		List<IndustryDTO> maincategory = industryDB.maincategory(dongCode);
-		// ModelAndView Setting
+		System.out.println(sigunguCode);
+		
+		sigungu = areaDB.sigungu(sigunguCode);
+		dong = areaDB.dong(dongCode);
+		
+		sigunguName = sigungu.getName();
+		dongName = dong.getName();
+		
+		List<MainCategoryDTO> MainList = industryDB.category_mainList();
+		mv.addObject("main", MainList);
+		mv.addObject("dongName", dongName);
+		mv.addObject("sigunguName", sigunguName);
+		mv.addObject("dongCode", dongCode);
+		mv.addObject("name" , name);
+		mv.addObject("userid", userid);
+		
 		mv.setViewName("jsp_nohead/dashBoard");
-		mv.addObject("sigungu", sigungu);
-		mv.addObject("dong", dong);
-		mv.addObject("maincategory", maincategory);
-
 		return mv;
-	}
 
-	@RequestMapping(value = "/douhnutchart", method = RequestMethod.POST, produces = "application/json; charset=utf8")
+			
+		
+
+	}
+	
+	@RequestMapping(value = "boardWriteForm/{dongCode}" , produces = "application/text; charset=utf8")
 	@ResponseBody
-	public List<IndustryRankDTO> drawDouhnutChartOnDashBoard(@RequestBody Map<String, String> dongCode) {
-		// 선택된 동의 maincategoryRank (산업-대분류 랭킹 TOP3.)
-		List<IndustryRankDTO> maincategoryRank = industryDB.maincategoryRank(dongCode.get("code"));
+	public ModelAndView boardWriteForm(@PathVariable("dongCode") String dongCode) throws Throwable {
+		mv = new ModelAndView();
+		String dongName = "";
+		String sigunguName = "";
+		AreaDTO sigungu = null;
+		AreaDTO dong=null;
 
-		return maincategoryRank;
+		String sigunguCode = dongCode.substring(0, 5);
+		
+		sigungu = areaDB.sigungu(sigunguCode);
+		dong = areaDB.dong(dongCode);
+		
+		sigunguName = sigungu.getName();
+		dongName = dong.getName();
+		
+		mv.addObject("dongName", dongName);
+		mv.addObject("sigunguName", sigunguName);
+		mv.addObject("dongCode", dongCode);
+		mv.addObject("userid", userid);
+		mv.addObject("name", name);
+		mv.setViewName("jsp_nohead/boardWriteForm");
+		return mv;
+			
 	}
-
-	@RequestMapping(value = "/barchart", method = RequestMethod.POST, produces = "application/json; charset=utf8")
+	
+	@RequestMapping(value = "myArea/{userid}" , produces = "application/text; charset=utf8")
 	@ResponseBody
-	public List<IndustryRankDTO> drawBarChartOnDashBoard(@RequestBody Map<String, String> dongCode) {
-		// 선택된 동의 middlecategoryRank (산업-중분류 랭킹 TOP3.)
-		List<IndustryRankDTO> middlecategoryRank = industryDB.middlecategoryRank(dongCode.get("code"));
+	public ModelAndView myArea(@PathVariable("userid") String userid) throws Throwable {
+		mv = new ModelAndView();
+		String dongName = "";
+		String sigunguName = "";
+		AreaDTO sigungu = null;
+		AreaDTO dong=null;
 
-		return middlecategoryRank;
+		List<Object> areaLike = arealikeDB.selectAreaLike(userid);
+		
+		/*String sigunguCode = dong.substring(0, 5);
+		
+		sigungu = areaDB.sigungu(sigunguCode);
+		dong = areaDB.dong(dong);
+		
+		sigunguName = sigungu.getName();
+		dongName = dong.getName();*/
+		
+		mv.addObject("dongName", dongName);
+		mv.addObject("sigunguName", sigunguName);
+		mv.addObject("dongCode", dong);
+		mv.addObject("userid", userid);
+		mv.addObject("name", name);
+		mv.setViewName("jsp_nohead/boardWriteForm");
+		return mv;
+
+			
+		
+
 	}
-
-	@RequestMapping(value = "/frenchise", method = RequestMethod.POST, produces = "application/json; charset=utf8")
-	@ResponseBody
-	public List<IndustryRankDTO> frenchiseRank(@RequestBody Map<String, String> dongCode) {
-		List<IndustryRankDTO> frenchiseRank = industryDB.frenchiseRank(dongCode.get("code"));
-
-		return frenchiseRank;
-	}
-
-	@RequestMapping(value = "/middlecategory", method = RequestMethod.POST, produces = "application/json; charset=utf8")
-	@ResponseBody
-	public List<IndustryDTO> middlecategory(@RequestBody Map<String, String> data) {
-		List<IndustryDTO> middlecategory = industryDB.middlecategory(data);
-
-		return middlecategory;
-	}
-
-	@RequestMapping(value = "/smallcategory", method = RequestMethod.POST, produces = "application/json; charset=utf8")
-	@ResponseBody
-	public List<IndustryDTO> smallcategory(@RequestBody Map<String, String> data) {
-		List<IndustryDTO> smallcategory = industryDB.smallcategory(data);
-
-		return smallcategory;
-	}
-
-	@RequestMapping(value = "/currentDongStore", method = RequestMethod.POST, produces = "application/json; charset=utf8")
-	@ResponseBody
-	public List<StoreDTO> currentDongStore(@RequestBody Map<String, String> data) {
-		System.out.println(data);
-		// data -> 위도경도 범위 & 동 코드 & 업종분류 및 코드
-		List<StoreDTO> storeList = storeDB.storeListInDongBound(data);
-		return storeList;
-	}
-
 }
